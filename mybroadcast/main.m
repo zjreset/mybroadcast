127c3557f8cd3978a7bdf32f887a9327e95f36cd
runes <runes@runes-MacBook-Pro.local>
2012-12-05 18:17:19 +0800

    Initial Commit

---
+++ mybroadcast/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mybroadcast
+//
+//  Created by runes on 12-12-5.
+//  Copyright (c) 2012年 runes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
